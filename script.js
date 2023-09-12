0005bbc885b7f9b54a0abdb33d77df100fa72f21
FVaganov <f.vaganov@bk.ru>
2023-09-12 21:57:36 +0300

    4

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
-function processQueueOperations(operations) {
-    let queue = [];
-    let worriedCount = 0;
-  
-    for (let i = 0; i < operations.length; i++) {
-      let operation = operations[i].split(" ");
-      let command = operation[0];
-  
-      if (command === "WORRY") {
-        let index = parseInt(operation[1]);
-        queue[index] = "worried";
-        worriedCount++;
-      } else if (command === "QUIET") {
-        let index = parseInt(operation[1]);
-        queue[index] = undefined;
-        worriedCount--;
-      } else if (command === "COME") {
-        let count = parseInt(operation[1]);
-        if (count > 0) {
-          for (let j = 0; j < count; j++) {
-            queue.push(undefined);
-          }
-        } else {
-          queue.splice(count);
-          worriedCount -= queue.slice(count).filter((element) => element === "worried").length;
-        }
-      } else if (command === "WORRY_COUNT") {
-        console.log(worriedCount);
-      }
-    }
-  }
-  
-  let operations = [
-    "COME 5",
-    "WORRY 1",
-    "WORRY 4",
-    "COME -2",
-    "WORRY_COUNT",
-    "COME 3",
-    "WORRY 3",
-    "WORRY_COUNT"
-  ];
-  
-  processQueueOperations(operations);
+function queueOperations(commands){
+  let queue = new Array(100).fill(0);
+
+   for (let i=0; i <commands.length; i++){
+       let parts = commands[i].split(" ");
+       let command=parts[0];
+
+       if (command==="COME"){
+           let number = parseInt(parts[1]);
+           if (number>0){
+               queue = queue.concat(new Array(number).fill(0));
+           }
+           else{
+               queue=queue.slice(0, number);
+           }
+       } 
+       else if (command ==="WORRY"){
+           let number=parseInt(parts[1]);
+           queue[number]=1;
+       }
+       else if (command === "QUIET"){
+           let number = parseInt(parts[1]);
+           queue[number]=0;
+       }
+       else if (command==="WORRY_COUNT"){
+           let count = queue.filter(person =>person ===1).length;
+           console.log(count)
+       }
+   }
+}
+let commands=["COME 5", "WORRY 1", "WORRY_COUNT", "WORRY 3", "WORRY_COUNT", "QUIET 1", "WORRY_COUNT"];
+queueOperations(commands)
